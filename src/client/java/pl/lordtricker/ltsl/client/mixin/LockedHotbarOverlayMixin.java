30be7e2e4d271fd0254117d15eab29ddccf8a363
LordTricker <oskar@blazecode.pl>
2025-04-18 14:40:00 +0200

    fixed itemFrame blocking

--- src/client/java/pl/lordtricker/ltsl/client/mixin/LockedHotbarOverlayMixin.java
+++ src/client/java/pl/lordtricker/ltsl/client/mixin/LockedHotbarOverlayMixin.java
@@ -15,7 +15,7 @@ import pl.lordtricker.ltsl.client.LtslotlockClient;
 @Mixin(InGameHud.class)
 public class LockedHotbarOverlayMixin {
 
-    private static final Identifier LOCK_ICON = new Identifier("ltsl", "textures/gui/lock_icon.png");
+    private static final Identifier LOCK_ICON = new Identifier("ltsl", "textures/gui/lock.png");
 
     @Inject(method = "renderHotbar", at = @At("HEAD"))
     private void onRenderHotbar(float tickDelta, MatrixStack matrices, CallbackInfo ci) {
