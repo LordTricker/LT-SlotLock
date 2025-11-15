40c93972fa3c193268d6a6a46b405277167ba09e
LordTricker <oskar@blazecode.pl>
2025-11-15 13:45:17 +0100

    ad config only

--- src/client/java/pl/lordtricker/ltsl/client/config/ConfigLoader.java
+++ src/client/java/pl/lordtricker/ltsl/client/config/ConfigLoader.java
@@ -3,6 +3,7 @@ package pl.lordtricker.ltsl.client.config;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.loader.api.FabricLoader;
+import static pl.lordtricker.ltsl.client.config.SlotSettings.adsEnabled;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -56,6 +57,7 @@ public class ConfigLoader {
     private static SettingsConfig createDefaultConfig() {
         SettingsConfig cfg = new SettingsConfig();
         cfg.slotSettings.doNotCleanSlots = List.of();
+        cfg.adsEnabled = adsEnabled;
         cfg.slotSettings.activeSlotHex = "#80ccff";
         cfg.slotSettings.blockedSlotHex = "#80cc00";
         cfg.slotLockEnabled = true;
