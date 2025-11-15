d8d67e3cddee597392f4a0ce6d501103ae733018
LordTricker <oskar@blazecode.pl>
2025-11-15 12:52:37 +0100

    ad config only

--- src/client/java/pl/lordtricker/ltsl/client/config/SettingsConfig.java
+++
@@ -1,7 +0,0 @@
-package pl.lordtricker.ltsl.client.config;
-
-public class SettingsConfig {
-    public SlotSettings slotSettings = new SlotSettings();
-    public boolean slotLockEnabled = true;
-    public boolean itemFrameLockEnabled = true;
-}
